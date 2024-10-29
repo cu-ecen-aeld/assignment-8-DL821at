fbaf07dfe7e79ef1ce2347bd60ae87c783bec182
TLee <tale1433@colorado.edu>
2024-10-29 10:16:23 +0000

    resolve port conflict

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -9,21 +9,25 @@ AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-DL82
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-# Define the server directory where the aesdsocket Makefile is located
-AESDSOCKET_SERVER_DIR := /home/dleevm/assignment-1-tale1433/server
-
-# Cross-compile aesdsocket from the server subdirectory
+# Build both finder-app and server (assuming both are part of the aesd-assignments package)
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) -C $(AESDSOCKET_SERVER_DIR)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# Install the aesdsocket binary and the start-stop script
+# Install the aesdsocket binary, start-stop script, and finder-app configurations
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# Install the compiled aesdsocket binary to /usr/bin on the target
-	$(INSTALL) -D -m 755 $(AESDSOCKET_SERVER_DIR)/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
 
 	# Install the aesdsocket-start-stop script to /etc/init.d/S99aesdsocket on the target
-	$(INSTALL) -D -m 755 $(AESDSOCKET_SERVER_DIR)/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
+	# Install the finder-app configuration files and scripts
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/* $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
