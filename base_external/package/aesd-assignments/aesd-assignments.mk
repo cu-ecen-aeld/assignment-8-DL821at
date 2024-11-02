08284658ac57132e74ee647b60cbecb2d04b2ba6
TLee <tale1433@colorado.edu>
2024-11-02 02:57:34 +0000

    try resolve actions-failure

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -18,16 +18,15 @@ endef
 # Install the aesdsocket binary, start-stop script, and finder-app configurations
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# Install the compiled aesdsocket binary to /usr/bin on the target
-	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 
 	# Install the aesdsocket-start-stop script to /etc/init.d/S99aesdsocket on the target
 	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
 	# Install the finder-app configuration files and scripts
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	#$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	#$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/* $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
