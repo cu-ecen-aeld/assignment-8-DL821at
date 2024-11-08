c68e8aa8a288a2678a9e16d79a72dc99d18c059e
dlee_debian <darrenlee821@gmail.com>
2024-11-09 03:41:09 +0800

    update commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 85fe82931b0bf640191b20fbdb3a6b5e8f8ee8c8
+AESD_ASSIGNMENTS_VERSION = 391e2eaeff7a4415b84c81368efefb660628809b
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-DL821at.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
