5739d625178947a72d2d72a9df4cde61fc9986a3
dlee_debian <darrenlee821@gmail.com>
2025-03-05 00:08:59 +0800

    modify ldd.mk to include MAKE OPTS commands

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,6 +4,7 @@ LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS += scull
 LDD_MODULE_SUBDIRS += misc-modules
+LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
