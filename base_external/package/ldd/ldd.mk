2e9a63207969f34eab43b29c5d0513ee71e27a7b
dlee_debian <darrenlee821@gmail.com>
2025-03-06 23:57:57 +0800

    revert aesd_qemu_defconfig back change ldd.mk for scull misc-modules priority

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -2,8 +2,9 @@ LDD_VERSION = 'd9c8eeb916f4f39658888e92d6c8e05cd437882e'
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-DL821at.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS = scull
-LDD_MODULE_SUBDIRS += misc-modules
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull 
 LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
 
 $(eval $(kernel-module))
