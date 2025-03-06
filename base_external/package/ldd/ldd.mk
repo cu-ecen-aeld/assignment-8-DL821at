4cc61364d7ff16a994f7ebe82e610300cbff91c5
dlee_debian <darrenlee821@gmail.com>
2025-03-07 02:59:36 +0800

    ldd.mk revert

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -3,8 +3,8 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-DL821at.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules
-LDD_MODULE_SUBDIRS += scull 
+LDD_MODULE_SUBDIRS = scull
+LDD_MODULE_SUBDIRS += misc-modules  
 LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
 
 $(eval $(kernel-module))
